b0f99f9d5ec3f6886cd4187dd94b8ee67d081299
Shiv Patel <shivpatel0204@gmail.com>
2024-10-20 20:59:27 -0400

    WIP

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 //Task 2: Add event Listeners for Product Selection
 const priceElement = document.getElementById("product-price");
-const sizeSelector = document.getElementById("size-selector")
+const sizeSelector = document.getElementById("size-selector");
+const formResponse = document.getElementById("for,-reponse");
 
 sizeSelector.addEventListener("change" , (event) => {
     const selectedPrice = event.target.value;
@@ -9,4 +10,10 @@ sizeSelector.addEventListener("change" , (event) => {
 
 
 //Task 3: Handle Stock Availability
-let availableStock = [10, 8, 12, 20]
+let availableStock = [10, 8, 12, 0]
+
+if (sizeSelector.availableStock > 0) { // if stock is above ) from availableStock array then the purchase will go through
+        formResponse.textContent = "Thank you for your purchas e!"
+    } else { 
+        formResponse.textContent = "Please select another size!"
+      }
