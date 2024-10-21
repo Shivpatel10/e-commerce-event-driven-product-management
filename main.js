da5757d1cc3775c5ac4c58fade21fb201c79fe70
Shiv Patel <shivpatel0204@gmail.com>
2024-10-20 20:47:34 -0400

    WIP

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//Task 2:Add event Listeners for Product Selection
+//Task 2: Add event Listeners for Product Selection
 const priceElement = document.getElementById("product-price");
 const sizeSelector = document.getElementById("size-selector")
 
@@ -6,3 +6,7 @@ sizeSelector.addEventListener("change" , (event) => {
     const selectedPrice = event.target.value;
     priceElement.textContent = `$${selectedPrice}`
 })
+
+
+//Task 3: Handle Stock Availability
+let availableStock = [10, 8, 12, 20]
