01a3b4c26d1b65ca338cf7742f7d55688ccdcbdf
Shiv Patel <shivpatel0204@gmail.com>
2024-10-20 21:04:54 -0400

    WIP

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 const priceElement = document.getElementById("product-price");
 const sizeSelector = document.getElementById("size-selector");
 const formResponse = document.getElementById("for,-reponse");
+const purchaseButton = document.getElementById("purchase-button");
+
 
 sizeSelector.addEventListener("change" , (event) => {
     const selectedPrice = event.target.value;
@@ -12,8 +14,8 @@ sizeSelector.addEventListener("change" , (event) => {
 //Task 3: Handle Stock Availability
 let availableStock = [10, 8, 12, 0]
 
-if (sizeSelector.availableStock > 0) { // if stock is above ) from availableStock array then the purchase will go through
-        formResponse.textContent = "Thank you for your purchas e!"
+if (availableStock > 0) { // if stock is above ) from availableStock array then the purchase will go through
+        formResponse.textContent = "Thank you for your purchase!"
     } else { 
         formResponse.textContent = "Please select another size!"
       }
